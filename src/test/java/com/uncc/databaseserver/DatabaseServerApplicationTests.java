4ee672982fb39b706ef31d8e5d7ab681f0ebb93e
SlamDunk <1430701240@qq.com>
2018-10-29 20:39:03 -0400

    Initial commit

---
+++ src/test/java/com/uncc/databaseserver/DatabaseServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uncc.databaseserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DatabaseServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
